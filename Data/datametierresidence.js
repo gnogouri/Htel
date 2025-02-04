e22b1a88b97a137e44831ff1417cc4f0602849f0
gnogouri <gnogourizebehi01@gmail.com>
2025-02-04 11:08:27 +0000

    deuxime modif

--- Data/datametierresidence.js
+++ Data/datametierresidence.js
@@ -3,12 +3,12 @@ export const residences = [
     title: 'NOS RESIDENCES',
     images: [
    
-      { id: 1, src: require('../Assets/IMG/MetierAccueil/luxe1.jpeg'), title: 'CH 1' },
-      { id: 2, src: require('../Assets/IMG/MetierAccueil/luxe2.jpg'), title: 'CH 2' },
-      { id: 3, src: require('../Assets/IMG/MetierAccueil/lux3.jpeg'), title: 'CH 3' },
-      { id: 4, src: require('../Assets/IMG/MetierAccueil/luxe4.jpg'), title: 'CH 4' },
-      { id: 5, src: require('../Assets/IMG/MetierAccueil/luxe5.jpg'), title: 'CH 5' },
-      { id: 6, src: require('../Assets/IMG/MetierAccueil/luxe6.jpg'), title: 'CH 6' },
+      { id: 1, src: require('../Assets/IMG/MetierAccueil/luxe1.jpeg'), title: 'RESIDENCE 1' },
+      { id: 2, src: require('../Assets/IMG/MetierAccueil/luxe2.jpg'), title: 'RESIDENCE 2' },
+      { id: 3, src: require('../Assets/IMG/MetierAccueil/lux3.jpeg'), title: 'RESIDENCE 3' },
+      { id: 4, src: require('../Assets/IMG/MetierAccueil/luxe4.jpg'), title: 'RESIDENCE 4' },
+      { id: 5, src: require('../Assets/IMG/MetierAccueil/luxe5.jpg'), title: 'RESIDENCE 5' },
+      { id: 6, src: require('../Assets/IMG/MetierAccueil/luxe6.jpg'), title: 'RESIDENCE 6' },
       { id: 7, src: require('../Assets/IMG/MetierAccueil/luxe7.jpg'), title: 'CH 7' },
       { id: 8, src: require('../Assets/IMG/MetierAccueil/luxe8.jpg'), title: 'CH 8' },
       { id: 9, src: require('../Assets/IMG/MetierAccueil/luxe9.jpg'), title: 'CH 9' },
