d6dd8310caa6b5ec32d25476f3002f5808bae204
gnogouri <gnogourizebehi01@gmail.com>
2025-02-10 14:09:54 +0000

    modif du 07 02

--- scran/Acceuil.js
+++ scran/Acceuil.js
@@ -2,26 +2,15 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import Accueil from './Metier/Accueil';
 const Tab = createBottomTabNavigator();
 const Acceuil = () => {
   return (
   
       <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-
-            if (route.name === 'Accueil') {
-              iconName = focused ? 'home' : 'home-outline';
-            } else if (route.name === 'Favoris') {
-              iconName = focused ? 'star' : 'star-outline';
-            } else if (route.name === 'Profil') {
-              iconName = focused ? 'person' : 'person-outline';
-            }
-
-            
-          },
+        screenOptions={() => ({
+          tabBarIcon: ({ focused, color, size }) => {},
           tabBarActiveTintColor: '#007bff',
           tabBarInactiveTintColor: 'gray',
           tabBarStyle: { backgroundColor: '#6e2b04' },
@@ -30,12 +19,14 @@ const Acceuil = () => {
         <Tab.Screen
           name="Accueil"
           component={Accueil}
-          options={{ headerShown: false }}
+          options={{headerShown: false,
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name="home" size={size} color={color} />
+            ),
+          }}
+
         />
         <Tab.Screen name="les chambres" component={Accueil} />
-        {/* <Tab.Screen name="les chambres" component={Accueil} /> */}
-        {/* <Tab.Screen name="Favoris" component={Favoris} />
-        <Tab.Screen name="Profil" component={Profil} /> */}
       </Tab.Navigator>
  
   )
